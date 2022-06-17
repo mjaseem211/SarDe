b104ad419366df8b21aabe74b14317750fb1dce1
jaseemtirur7@gmail.com <jaseemtirur7@gmail.com>
2022-06-17 15:38:06 +0530

    quality check edits

--- sarDe/lib/screens/Job_Main/Quality_Check/Add_new_Quality/location_dropdown.dart
+++ sarDe/lib/screens/Job_Main/Quality_Check/Add_new_Quality/location_dropdown.dart
@@ -1,67 +1,67 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
-
-// ignore: camel_case_types
-class location_drop_down extends StatefulWidget {
-  const location_drop_down({Key? key}) : super(key: key);
-
-  @override
-  State<location_drop_down> createState() => _location_drop_downState();
-}
-
-// ignore: camel_case_types
-class _location_drop_downState extends State<location_drop_down> {
-  String? dropdownValue;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      height: 35.h,
-      width: 304.w,
-      decoration: BoxDecoration(
-          border: Border.all(color: const Color(0xffDD7164), width: 1.w),
-          borderRadius: BorderRadius.circular(4.r)),
-      child: DropdownButton<String>(
-        value: dropdownValue,
-        hint: Padding(
-          padding: EdgeInsets.only(left: 19.w),
-          child: Text(
-            'location',
-            style: TextStyle(
-              color: const Color(0xff000000).withOpacity(0.26),
-            ),
-          ),
-        ),
-        isExpanded: true,
-        icon: const Icon(
-          Icons.keyboard_arrow_down_outlined,
-          color: Color(0xffDD7164),
-        ),
-        elevation: 16,
-        underline: Container(
-          color: Colors.white,
-        ),
-        style: TextStyle(color: const Color(0xff000000).withOpacity(0.26)),
-        onChanged: (String? newValue) {
-          setState(() {
-            dropdownValue = newValue!;
-          });
-        },
-        items: <String>['kuttichira', 'maradu', 'nadakkav', 'meenchanda']
-            .map<DropdownMenuItem<String>>((String value) {
-          return DropdownMenuItem<String>(
-            value: value,
-            child: Padding(
-                padding: EdgeInsets.only(left: 19.w),
-                child: Text(
-                  value,
-                  style: TextStyle(
-                    color: const Color(0xff000000).withOpacity(0.26),
-                  ),
-                )),
-          );
-        }).toList(),
-      ),
-    );
-  }
-}
+// import 'package:flutter/material.dart';
+// import 'package:flutter_screenutil/flutter_screenutil.dart';
+//
+// // ignore: camel_case_types
+// class location_drop_down extends StatefulWidget {
+//   const location_drop_down({Key? key}) : super(key: key);
+//
+//   @override
+//   State<location_drop_down> createState() => _location_drop_downState();
+// }
+//
+// // ignore: camel_case_types
+// class _location_drop_downState extends State<location_drop_down> {
+//   String? dropdownValue;
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container(
+//       height: 35.h,
+//       width: 304.w,
+//       decoration: BoxDecoration(
+//           border: Border.all(color: const Color(0xffDD7164), width: 1.w),
+//           borderRadius: BorderRadius.circular(4.r)),
+//       child: DropdownButton<String>(
+//         value: dropdownValue,
+//         hint: Padding(
+//           padding: EdgeInsets.only(left: 19.w),
+//           child: Text(
+//             'location',
+//             style: TextStyle(
+//               color: const Color(0xff000000).withOpacity(0.26),
+//             ),
+//           ),
+//         ),
+//         isExpanded: true,
+//         icon: const Icon(
+//           Icons.keyboard_arrow_down_outlined,
+//           color: Color(0xffDD7164),
+//         ),
+//         elevation: 16,
+//         underline: Container(
+//           color: Colors.white,
+//         ),
+//         style: TextStyle(color: const Color(0xff000000).withOpacity(0.26)),
+//         onChanged: (String? newValue) {
+//           setState(() {
+//             dropdownValue = newValue!;
+//           });
+//         },
+//         items: <String>['kuttichira', 'maradu', 'nadakkav', 'meenchanda']
+//             .map<DropdownMenuItem<String>>((String value) {
+//           return DropdownMenuItem<String>(
+//             value: value,
+//             child: Padding(
+//                 padding: EdgeInsets.only(left: 19.w),
+//                 child: Text(
+//                   value,
+//                   style: TextStyle(
+//                     color: const Color(0xff000000).withOpacity(0.26),
+//                   ),
+//                 )),
+//           );
+//         }).toList(),
+//       ),
+//     );
+//   }
+// }
